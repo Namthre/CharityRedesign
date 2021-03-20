0e727b793fde6590e094f1e657a8743a69492b17
Namthre <namthre@gmail.com>
2021-03-19 21:12:49 -0400

    Initial Commit

---
+++ LoginForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contact_Donation_Page
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
